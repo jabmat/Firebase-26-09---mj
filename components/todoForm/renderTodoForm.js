9929dd541a5d1487164547429b381d8089fc62ee
Mateusz Jablonski <mjablonski.praca@gmail.com>
2022-09-29 20:32:06 +0200

    godzina do końca - chorowitek

---
+++ components/todoForm/renderTodoForm.js
@@ -0,0 +1,26 @@
+// W funkcji:
+// 1. Stwórz element <form> i nadaj mu id 'todo-form'
+// 2. Stwórz element <input>, type "text", id "todo-input"
+// 3. Stwórz element <fieldset>, id "todo-fieldset"
+// 4. Stwórz element <legend>, id 'todo-legend', textCotnent 'Select a category'
+// 5. Będą 4 kategorie: work, life, sport, education
+// - tworzycie diva i nazywacie go w zaleznosci od kategorii (np. divWork, divLife) i id też od kategorii ("div-work", "div-life")
+// - do środka diva wrzucacie input (type "radio", id "radio-{kategoria (np. work)}" ("radio-work"), name "category"!!!, value "kategoria (np. work, life)")
+// oraz label, for "radio-{kategoria}" (atrybut for labela musi mieć tą samą wartość co id inputu), textContent
+// 6. Do elementu <fieldset> przy pomocy appendChild, podpinacie legend i wszystkie 4 divy
+// 7. Stwórz element <button>, type 'submit', id "todo-form-submit-button", textContent "Add todo"
+// 8. Do <form> podpinacie input (pkt 2), fieldset i submit button
+// 9. Cały form returnujecie
+
+/* <div id="div-work">
+    <input type="radio" id="radio-work" name="category" value="work"/>
+    <label for="radio-work">Work</label>
+</div>
+<div id="div-life">
+    <input type="radio" id="radio-life" name="category" value="life"/>
+    <label for="radio-life">Life</label>
+</div> */
+
+// export default () {
+
+// }
